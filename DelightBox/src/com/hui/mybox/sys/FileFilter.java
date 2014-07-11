ce7b5f1966c5dc70cd2b5f4923bb353e0fe4942a
hui.li <hui.li@dragonflow.com>
2014-07-11 14:32:00 +0800

    dd

--- DelightBox/src/com/hui/mybox/sys/FileFilter.java
+++ DelightBox/src/com/hui/mybox/sys/FileFilter.java
@@ -43,7 +43,7 @@ public class FileFilter implements Runnable {
 	
 	@Override
 	public void run() {
-		LogUtil.debug(TAG, "FileFilter is run... ");
+		LogUtil.debug(TAG, "FileFilter is run...");
 		// 获取根目录路径
 		try {
 			String rootPath = Environment.getExternalStorageDirectory()
