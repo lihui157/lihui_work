29760a8ec479e4e366f25b97459198ff6617b117
lihui-d <lihui-d@lihui-d-PC>
2014-11-10 00:15:54 +0800

    111

---
+++ myBox/src/com/jhgzs/utils/MediaUtil.java
@@ -0,0 +1,17 @@
+package com.jhgzs.utils;
+
+import java.io.IOException;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+public class MediaUtil {
+
+	
+
+}
